cf787a3f8bff5e8f026915c784e29e3ffcd0d296
Daniel Snider <danielsnider12@gmail.com>
2023-04-01 09:42:04 -0400

    Important fix to artifact run script

--- artifact/run.sh
+++ artifact/run.sh
@@ -7,15 +7,15 @@ set -x
 # Single GPU experiments
 export EXTRA_ARGS="--env HOTLINE_DATASET_DIR=/home/ubuntu/dataset --env CUDA_VISIBLE_DEVICES=0"
 # export EXTRA_ARGS=$EXTRA_ARGS" --env HOTLINE_BATCH_SIZE=4"
-# run resnet
-# run transformer
+run resnet
+run transformer
+run rnn
 
 # 4 x GPU experiments
 export EXTRA_ARGS="--env HOTLINE_DATASET_DIR=/home/ubuntu/dataset --env CUDA_VISIBLE_DEVICES=0,1,2,3"
-run rnn
 # export EXTRA_ARGS=$EXTRA_ARGS" --env HOTLINE_BATCH_SIZE=4"
-# run resnet
-# run transformer
+run resnet
+run transformer
 
 
 
